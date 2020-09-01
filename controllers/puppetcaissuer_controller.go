6cd356a2b3db5ebf1a16d51d8c55599573a8d409
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-09-01 09:28:49 +0200

    [WIP] Controller logic

--- controllers/puppetcaissuer_controller.go
+++ controllers/puppetcaissuer_controller.go
@@ -18,13 +18,15 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	certmanagerv1alpha1 "github.com/camptocamp/puppetca-issuer/api/v1alpha1"
+	api "github.com/camptocamp/puppetca-issuer/api/v1alpha1"
 )
 
 // PuppetCAIssuerReconciler reconciles a PuppetCAIssuer object
@@ -36,18 +38,115 @@ type PuppetCAIssuerReconciler struct {
 
 // +kubebuilder:rbac:groups=certmanager.puppetca,resources=puppetcaissuers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=certmanager.puppetca,resources=puppetcaissuers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 func (r *PuppetCAIssuerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("puppetcaissuer", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("puppetcaissuer", req.NamespacedName)
 
-	// your logic here
+	iss := new(api.PuppetCAIssuer)
+	if err := r.Client.Get(ctx, req.NamespacedName, iss); err != nil {
+		log.Error(err, "failed to retrieve PuppetCAIssuer resource")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	return ctrl.Result{}, nil
+	statusReconciler := newPuppetCAStatusReconciler(r, iss, log)
+	if err := validatePuppetCAIssuerSpec(iss.Spec); err != nil {
+		log.Error(err, "failed to validate PuppetCAIssuer resource")
+		statusReconciler.UpdateNoError(ctx, api.ConditionFalse, "Validation", "Failed to validate resource: %v", err)
+		return ctrl.Result{}, err
+	}
+
+	// Initialize and store the provisioner
+
+	// Puppet CA url, cert, key, and CA cert are all stored as secrets
+	var secret core.Secret
+	var ok bool
+	var url []byte
+	var cert []byte
+	var key []byte
+	var caCert []byte
+
+	secretNamespaceName := types.NamespacedName{
+		Namespace: req.Namespace,
+		Name:      iss.Spec.Provisioner.Name,
+	}
+
+	if err := r.Client.Get(ctx, secretNamespaceName, &secret); err != nil {
+		log.Error(err, "failed to retrieve Puppet CA secrets", "namespace", secretNamespaceName.Namespace, "name", secretNamespaceName.Name)
+		if apierrors.IsNotFound(err) {
+			statusReconciler.UpdateNoError(ctx, api.ConditionFalse, "NotFound", "Failed to retrieve Puppet CA secrets: %v", err)
+		} else {
+			statusReconciler.UpdateNoError(ctx, api.ConditionFalse, "Error", "Failed to retrieve Puppet CA secrets: %v", err)
+		}
+		return ctrl.Result{}, err
+	}
+
+	url, ok = secret.Data[iss.Spec.Provisioner.URLRef.Key]
+	if !ok {
+		err := fmt.Errorf("secret %s does not contain key %s", secret.Name, iss.Spec.Provisioner.URLRef.Key)
+		log.Error(err, "failed to retrieve Puppet CA URL from secret", "namespace", secretNamespaceName.Namespace, "name", secretNamespaceName.Name)
+		statusReconciler.UpdateNoError(ctx, api.ConditionFalse, "NotFound", "Failed to retrieve Puppet CA URL from secret: %v", err)
+		return ctrl.Result{}, err
+	}
+
+	cert, ok = secret.Data[iss.Spec.Provisioner.CertRef.Key]
+	if !ok {
+		err := fmt.Errorf("secret %s does not contain key %s", secret.Name, iss.Spec.Provisioner.CertRef.Key)
+		log.Error(err, "failed to retrieve Puppet CA certificate from secret", "namespace", secretNamespaceName.Namespace, "name", secretNamespaceName.Name)
+		statusReconciler.UpdateNoError(ctx, api.ConditionFalse, "NotFound", "Failed to retrieve Puppet CA certificate from secret: %v", err)
+		return ctrl.Result{}, err
+	}
+
+	key, ok = secret.Data[iss.Spec.Provisioner.KeyRef.Key]
+	if !ok {
+		err := fmt.Errorf("secret %s does not contain key %s", secret.Name, iss.Spec.Provisioner.KeyRef.Key)
+		log.Error(err, "failed to retrieve Puppet CA key from secret", "namespace", secretNamespaceName.Namespace, "name", secretNamespaceName.Name)
+		statusReconciler.UpdateNoError(ctx, api.ConditionFalse, "NotFound", "Failed to retrieve Puppet CA key from secret: %v", err)
+		return ctrl.Result{}, err
+	}
+
+	caCert, ok = secret.Data[iss.Spec.Provisioner.CaCertRef.Key]
+	if !ok {
+		err := fmt.Errorf("secret %s does not contain key %s", secret.Name, iss.Spec.Provisioner.CaCertRef.Key)
+		log.Error(err, "failed to retrieve Puppet CA CA certificate from secret", "namespace", secretNamespaceName.Namespace, "name", secretNamespaceName.Name)
+		statusReconciler.UpdateNoError(ctx, api.ConditionFalse, "NotFound", "Failed to retrieve Puppet CA CA certificate from secret: %v", err)
+		return ctrl.Result{}, err
+	}
+
+	p := provisioners.NewProvisioner(string(url), string(cert),
+		string(key), string(caCert))
+
+	issNamespaceName := types.NamespacedName{
+		Namespace: req.Namespace,
+		Name:      req.Name,
+	}
+
+	provisioners.Store(issNamespaceName, p)
+
+	return ctrl.Result{}, statusReconciler.Update(ctx, api.ConditionTrue, "Verified", "PuppetCAIssuer verified and ready to sign certificates")
 }
 
 func (r *PuppetCAIssuerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&certmanagerv1alpha1.PuppetCAIssuer{}).
 		Complete(r)
+}
+
+func validatePuppetCAIssuerSpec(s api.PuppetCAIssuerSpec) error {
+	switch {
+	case s.Provisioner.Name == "":
+		return fmt.Errorf("spec.provisioner.name cannot be empty")
+	case s.Provisioner.URLRef.Key == "":
+		return fmt.Errorf("spec.provisioner.url.key cannot be empty")
+	case s.Provisioner.CertRef.Key == "":
+		return fmt.Errorf("spec.provisioner.cert.key cannot be empty")
+	case s.Provisioner.KeyRef.Key == "":
+		return fmt.Errorf("spec.provisioner.key.key cannot be empty")
+	case s.Provisioner.CaCertRef.Key == "":
+		return fmt.Errorf("spec.provisioner.cacert.key cannot be empty")
+	default:
+		return nil
+	}
 }
