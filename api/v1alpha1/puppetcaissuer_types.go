2aaf46d7809553d5224c6090ba00cf929c9195f4
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-09-16 11:08:50 +0200

    WIP: add missing conditions

--- api/v1alpha1/puppetcaissuer_types.go
+++ api/v1alpha1/puppetcaissuer_types.go
@@ -36,6 +36,9 @@ type PuppetCAIssuerSpec struct {
 type PuppetCAIssuerStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// +optional
+	Conditions []PuppetCAIssuerCondition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
@@ -81,6 +84,60 @@ type PuppetCAProvisioner struct {
 
 	// Reference to certificate to access the Puppet CA
 	CaCert SecretKeySelector `json:"cacert"`
+}
+
+// ConditionType represents a PuppetCAIssuer condition type.
+// +kubebuilder:validation:Enum=Ready
+type ConditionType string
+
+const (
+	// ConditionReady indicates that a PuppetCAIssuer is ready for use.
+	ConditionReady ConditionType = "Ready"
+)
+
+// ConditionStatus represents a condition's status.
+// +kubebuilder:validation:Enum=True;False;Unknown
+type ConditionStatus string
+
+// These are valid condition statuses. "ConditionTrue" means a resource is in
+// the condition; "ConditionFalse" means a resource is not in the condition;
+// "ConditionUnknown" means kubernetes can't decide if a resource is in the
+// condition or not. In the future, we could add other intermediate
+// conditions, e.g. ConditionDegraded.
+const (
+	// ConditionTrue represents the fact that a given condition is true
+	ConditionTrue ConditionStatus = "True"
+
+	// ConditionFalse represents the fact that a given condition is false
+	ConditionFalse ConditionStatus = "False"
+
+	// ConditionUnknown represents the fact that a given condition is unknown
+	ConditionUnknown ConditionStatus = "Unknown"
+)
+
+// AWSCMIssuerCondition contains condition information for the issuer.
+type PuppetCAIssuerCondition struct {
+	// Type of the condition, currently ('Ready').
+	Type ConditionType `json:"type"`
+
+	// Status of the condition, one of ('True', 'False', 'Unknown').
+	// +kubebuilder:validation:Enum=True;False;Unknown
+	Status ConditionStatus `json:"status"`
+
+	// LastTransitionTime is the timestamp corresponding to the last status
+	// change of this condition.
+	// +optional
+	LastTransitionTime *metav1.Time `json:"lastTransitionTime,omitempty"`
+
+	// Reason is a brief machine readable explanation for the condition's last
+	// transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+
+	// Message is a human readable description of the details of the last
+	// transition, complementing reason.
+	// +optional
+	Message string `json:"message,omitempty"`
 }
 
 func init() {
