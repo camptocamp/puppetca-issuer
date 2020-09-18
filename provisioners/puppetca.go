9ce115120cf2bc4490f95c56b7914e4cdc876cbf
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-09-18 13:32:05 +0200

    WIP: start impl

--- provisioners/puppetca.go
+++ provisioners/puppetca.go
@@ -18,6 +18,7 @@ package provisioners
 import (
 	"context"
 	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"sync"
 
@@ -36,7 +37,6 @@ type PuppetCA struct {
 }
 
 type PuppetCAProvisioner struct {
-	client *puppetca.Client
 	url    string
 	cert   string
 	key    string
@@ -69,106 +69,59 @@ func Store(namespacedName types.NamespacedName, provisioner *PuppetCAProvisioner
 // Sign sends the certificate requests to the Step CA and returns the signed
 // certificate.
 func (p *PuppetCAProvisioner) Sign(ctx context.Context, cr *certmanager.CertificateRequest) ([]byte, []byte, error) {
-	if p.client == nil {
-		client, err := puppetca.NewClient(p.url, p.key, p.cert, p.caCert)
-		if err != nil {
-			return nil, nil, fmt.Errorf("Failed to initialize Puppet CA client")
-		}
-		p.client = &client
+	// decode and check certificate request
+	csr, err := decodeCSR(cr.Spec.Request)
+	if err != nil {
+		return nil, nil, err
 	}
 
-	return nil, nil, fmt.Errorf("Failed to sign")
-	/*
-		// decode and check certificate request
-		csr, err := decodeCSR(cr.Spec.CSRPEM)
-		if err != nil {
-			return nil, nil, err
-		}
+	sans := append([]string{}, csr.DNSNames...)
+	for _, ip := range csr.IPAddresses {
+		sans = append(sans, ip.String())
+	}
 
-		sans := append([]string{}, csr.DNSNames...)
-		for _, ip := range csr.IPAddresses {
-			sans = append(sans, ip.String())
-		}
+	subject := csr.Subject.CommonName
+	if subject == "" {
+		subject = generateSubject(sans)
+	}
 
-		subject := csr.Subject.CommonName
-		if subject == "" {
-			subject = generateSubject(sans)
-		}
+	client, err := puppetca.NewClient(p.url, p.key, p.cert, p.caCert)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Failed to initialize Puppet CA client")
+	}
 
-		sess := session.Must(session.NewSession(&aws.Config{
-			MaxRetries: aws.Int(3),
-		}))
+	// Upload CSR
+	// err = client.SubmitCSR(csr)
 
-		svc := acmpca.New(sess, &aws.Config{
-			Region: aws.String(p.region),
-			Credentials: credentials.NewStaticCredentials(p.accesskey,
-				p.secretkey, ""),
-		})
+	// Sign cert
+	// err = client.SignCertByName(subject)
 
-		cparams := acmpca.IssueCertificateInput{
-			CertificateAuthorityArn: aws.String(p.arn),
-			SigningAlgorithm:        aws.String(acmpca.SigningAlgorithmSha256withrsa),
-			Csr:                     cr.Spec.CSRPEM,
-			Validity: &acmpca.Validity{
-				Type:  aws.String(acmpca.ValidityPeriodTypeDays),
-				Value: aws.Int64(int64(cr.Spec.Duration.Hours() / 24)),
-			},
-			IdempotencyToken: aws.String("awspca"),
-		}
+	// Download signed cert
+	certPem, err := client.GetCertByName(subject)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Error retrieving certificate")
+	}
 
-		output, err := svc.IssueCertificate(&cparams)
-
-		if err != nil {
-			return nil, nil, err
-		}
-
-		// wait for cert
-
-		cparams2 := acmpca.GetCertificateInput{
-			CertificateArn:          aws.String(*output.CertificateArn),
-			CertificateAuthorityArn: aws.String(p.arn),
-		}
-
-		svc.WaitUntilCertificateIssued(&cparams2)
-
-		output2, err2 := svc.GetCertificate(&cparams2)
-
-		if err2 != nil {
-			return nil, nil, err2
-		}
-
-		// Encode server certificate with the intermediate
-		certPem := []byte(*output2.Certificate + "\n")
-		chainPem := []byte(*output2.CertificateChain)
-
-		certPem = append(certPem, chainPem...)
-		return certPem, nil, nil
-	*/
-
-	return nil, nil, nil
+	return []byte(certPem), nil, nil
 }
 
 // decodeCSR decodes a certificate request in PEM format and returns the
 func decodeCSR(data []byte) (*x509.CertificateRequest, error) {
-	/*
-		block, rest := pem.Decode(data)
-		if block == nil || len(rest) > 0 {
-			return nil, fmt.Errorf("unexpected CSR PEM on sign request")
-		}
-		if block.Type != "CERTIFICATE REQUEST" {
-			return nil, fmt.Errorf("PEM is not a certificate request")
-		}
-		csr, err := x509.ParseCertificateRequest(block.Bytes)
-		if err != nil {
-			return nil, fmt.Errorf("error parsing certificate request: %v", err)
-		}
-		if err := csr.CheckSignature(); err != nil {
-			return nil, fmt.Errorf("error checking certificate request signature: %v", err)
-		}
-		return csr, nil
-	*/
-
-	return nil, nil
+	block, rest := pem.Decode(data)
+	if block == nil || len(rest) > 0 {
+		return nil, fmt.Errorf("unexpected CSR PEM on sign request")
+	}
+	if block.Type != "CERTIFICATE REQUEST" {
+		return nil, fmt.Errorf("PEM is not a certificate request")
+	}
+	csr, err := x509.ParseCertificateRequest(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing certificate request: %v", err)
+	}
+	if err := csr.CheckSignature(); err != nil {
+		return nil, fmt.Errorf("error checking certificate request signature: %v", err)
+	}
+	return csr, nil
 }
 
 // generateSubject returns the first SAN that is not 127.0.0.1 or localhost. The
@@ -176,17 +129,13 @@ func decodeCSR(data []byte) (*x509.CertificateRequest, error) {
 // are available `awspca-issuer-certificate` will be used as a subject is always
 // required.
 func generateSubject(sans []string) string {
-	/*
-		if len(sans) == 0 {
-			return "awspca-issuer-certificate"
+	if len(sans) == 0 {
+		return "awspca-issuer-certificate"
+	}
+	for _, s := range sans {
+		if s != "127.0.0.1" && s != "localhost" {
+			return s
 		}
-		for _, s := range sans {
-			if s != "127.0.0.1" && s != "localhost" {
-				return s
-			}
-		}
-		return sans[0]
-	*/
-
-	return ""
+	}
+	return sans[0]
 }
